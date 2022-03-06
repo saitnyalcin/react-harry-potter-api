c49fbdb80f01958ed455bc98237b022d84c873d8
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-03-06 16:28:20 -0500

    lottie loading animation is implemented

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
 import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+import animationData from "../src/lotties/leviosa.json";
 import "./App.css";
-import "./style.scss";
 import HarryPotter from "./components/HarryPotter";
+import "./style.scss";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 6000);
   }, []);
 
   return (
     <div className="App">
-      {loading ? <div className="loading">Loading...</div> : <HarryPotter />}
+      {loading ? (
+        <div className="loading">
+          <div>
+            <div>Loading...</div>
+            <Lottie options={defaultOptions} height={400} width={400} />
+            <div style={{ fontSize: "3rem", fontFamily: "Arial" }}>
+              It's LeviOsa not LeviosAAAA...
+            </div>
+          </div>
+        </div>
+      ) : (
+        <HarryPotter />
+      )}
     </div>
   );
 };
