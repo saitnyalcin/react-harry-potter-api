cdd74fed5bd9a8f23618141dae26fdf4b7b8857f
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-03-06 14:34:03 -0500

    loading screen is implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import './App.css';
-import './style.scss';
-import HarryPotter from './components/HarryPotter';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "./style.scss";
+import HarryPotter from "./components/HarryPotter";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <HarryPotter />
+      {loading ? <div className="loading">Loading...</div> : <HarryPotter />}
     </div>
   );
-}
+};
 
 export default App;
