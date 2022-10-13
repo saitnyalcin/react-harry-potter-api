c2d6f678fc60ea7617c1d67128de7a83d8a6e6f6
Steve Yalcin <steveyalcin@Steves-MacBook-Pro.local>
2022-10-13 15:26:20 -0400

    url is updated

--- src/services/characterService.js
+++ src/services/characterService.js
@@ -1,7 +1,7 @@
 const characterService = {
   API_URL() {
-    return 'http://hp-api.herokuapp.com/api/characters';
-  }
+    return "https://hp-api.herokuapp.com/api/characters";
+  },
 };
 
 export { characterService as default };
