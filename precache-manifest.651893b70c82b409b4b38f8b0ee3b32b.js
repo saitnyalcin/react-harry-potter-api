86811dc0ae4b8ec88436e2c9c462e08108d954c9
Sait N Yalcin <saitnyalcin@Saits-MacBook-Pro.local>
2022-12-11 13:39:38 -0500

    Updates

---
+++ precache-manifest.651893b70c82b409b4b38f8b0ee3b32b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "70d9a54d3baa2c98942a",
+    "url": "/react-harry-potter-api/static/css/main.e209e82c.chunk.css"
+  },
+  {
+    "revision": "70d9a54d3baa2c98942a",
+    "url": "/react-harry-potter-api/static/js/main.70d9a54d.chunk.js"
+  },
+  {
+    "revision": "eff0c3b950de86f211a2",
+    "url": "/react-harry-potter-api/static/js/1.eff0c3b9.chunk.js"
+  },
+  {
+    "revision": "86ca33b26ee3c64e9239",
+    "url": "/react-harry-potter-api/static/js/runtime~main.86ca33b2.js"
+  },
+  {
+    "revision": "457e29921fbd8f88202177bf3b4ad7f0",
+    "url": "/react-harry-potter-api/static/media/harry-potter-logo.457e2992.png"
+  },
+  {
+    "revision": "9e281b65bd0415e1a22226607b47d689",
+    "url": "/react-harry-potter-api/static/media/harry-potter-glass.9e281b65.png"
+  },
+  {
+    "revision": "4fbde265391a241e3ecb912c22ee2dfa",
+    "url": "/react-harry-potter-api/static/media/harry-potter-floating.4fbde265.png"
+  },
+  {
+    "revision": "0e9d704b089dc5909a8dba3f0f54669c",
+    "url": "/react-harry-potter-api/static/media/bg.0e9d704b.jpeg"
+  },
+  {
+    "revision": "bfa964941fd2347805a248f9533f7cf8",
+    "url": "/react-harry-potter-api/index.html"
+  }
+];
